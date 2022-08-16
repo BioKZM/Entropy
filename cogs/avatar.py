36135e0bbb45b06f36e00a61dfe08d611efb600e
BioKZM <qRnt@yandex.com>
2022-08-16 21:35:02 +0300

    test

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -5,8 +5,8 @@ class Avatar(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-    @commands.slash_command(name = disnake.Localized(key="{{AVATAR_NAME}}"), description = disnake.Localized(key = "{{AVATAR_DESCRIPTION}}"))
-    async def level(self,inter,sayı = commands.Param(name = "{{MEMBER_NAME}}",description = "{{MEMBER_DESCRIPTION}}")):
+    @commands.slash_command(name = disnake.Localized(key="AVATAR_NAME"), description = disnake.Localized(key = "AVATAR_DESCRIPTION"))
+    async def level(self,inter,sayı = commands.Param(name = disnake.Localized(key="MEMBER_NAME"),description = disnake.Localized(key = "MEMBER_DESCRIPTION"))):
         await inter.response.send_message("Test")
 
 
