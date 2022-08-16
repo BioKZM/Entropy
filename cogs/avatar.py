c2c1a479e31a72cd6629d66f0bdc14f1d425a955
BioKZM <qRnt@yandex.com>
2022-08-16 21:25:34 +0300

    test

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -1,23 +1,14 @@
 import disnake
-import json
 from disnake.ext import commands
-
-def getlang(client,message):
-    with open(f"guildOptions/{str(message.guild.id)}.json") as file:
-        data = json.load(file)
-    return data['lang']
 
 class Avatar(commands.Cog):
     def __init__(self,client):
         self.client = client
-        self.lang = (getlang)
 
-    @commands.slash_command(name = "avatar", description = "")
-    async def level(self,inter,sayı = commands.Param(
-        name = "sayı",
-        description = "Lütfen bir sayı gir!"
-    )):
-        await inter.response.send_message(self.lang)
+    @commands.slash_command(name = disnake.Localized(key="{{AVATAR_NAME}}"), description = disnake.Localized(key = "{{AVATAR_DESCRIPTION}}"))
+    async def level(self,inter,sayı = commands.Param(name = "{{MEMBER_NAME}}",description = "{{MEMBER_DESCRIPTION}}")):
+        await inter.response.send_message("Test")
+
 
 def setup(client):
     client.add_cog(Avatar(client))
