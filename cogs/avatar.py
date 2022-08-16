7800f358023be359c8c2b428a326b15cdad33dde
BioKZM <qRnt@yandex.com>
2022-08-16 21:56:14 +0300

    test

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -1,3 +1,4 @@
+from socket import timeout
 import disnake
 from disnake.ext import commands
 
@@ -5,9 +6,44 @@ class Avatar(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-    @commands.slash_command(name = disnake.Localized(key="AVATAR_NAME"), description = disnake.Localized(key = "AVATAR_DESCRIPTION"))
-    async def level(self,inter,sayı = commands.Param(name = disnake.Localized(key="MEMBER_NAME"),description = disnake.Localized(key = "MEMBER_DESCRIPTION"))):
-        await inter.response.send_message("Test")
+    @commands.slash_command(name = "avatar", description = "Display an user's avatar.")
+    async def avatar(self,inter,member:disnake.Member = None):
+        
+        if member == None:
+            member = inter.author
+
+
+
+        class GlobalView(disnake.ui.View):
+            @disnake.ui.button(label = "Global Avatar",style = disnake.ButtonStyle.green)
+            async def globalAvatar(self, inter, button:disnake.ui.Button):
+                embed = disnake.Embed(
+                    title = f"{member.name}'s avatar",
+                    description = "",
+                )
+                embed.set_image(url=member.avatar.url)
+                
+                await inter.response.edit_message(embed=embed,view=DisplayView(timeout=None))
+
+
+        class DisplayView(disnake.ui.View):
+            @disnake.ui.button(label = "Display Avatar",style = disnake.ButtonStyle.green)
+            async def displayAvatar(self,inter,button:disnake.ui.Button):
+                embed = disnake.Embed(
+                    title = f"{member.display_name}'s avatar",
+                    description = "",
+                )
+                embed.set_image(url=member.display_avatar.url)
+                await inter.response.edit_message(embed=embed,view=GlobalView(timeout=None))
+
+
+        embed = disnake.Embed(
+            title = f"{member.display_name}'s avatar",
+            description = "",
+        )
+        embed.set_image(url=member.display_avatar.url)
+
+        await inter.response.send_message(embed=embed,view=GlobalView(timeout=None))
 
 
 def setup(client):
