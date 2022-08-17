8c2563c7646e1a1dfff983895951d5a0b9bdb5ad
BioKZM <qRnt@yandex.com>
2022-08-17 17:36:35 +0300

    test

--- cogs/globalServerCount.py
+++ cogs/globalServerCount.py
@@ -1,7 +1,6 @@
 from pydoc import cli
 import disnake
 from disnake.ext import commands
-client = commands.Bot(command_prefix="!",intents = disnake.Intents.all(), help_command=None)
 class GlobalServerCount(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -11,17 +10,17 @@ class GlobalServerCount(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self,guild):
-        channel = client.get_channel(1009425217669582969)
+        channel = self.client.get_channel(1009425217669582969)
         await channel.send(f"**{guild}** sunucusuna davet edildim.")
-        channel = client.get_channel(1009434779508281385)
+        channel = self.client.get_channel(1009434779508281385)
         await channel.edit(name = f"Mevcut Sunucular : {len(self.client.guilds)}")
 
 
     @commands.Cog.listener()
     async def on_guild_remove(self,guild):
-        channel = client.get_channel(1009425217669582969)
+        channel = self.client.get_channel(1009425217669582969)
         await channel.send(f"**{guild}** sunucusundan atıldım.")
-        channel = client.get_channel(1009434779508281385)
+        channel = self.client.get_channel(1009434779508281385)
         await channel.edit(name = f"Mevcut Sunucular : {len(self.client.guilds)}")
 
 def setup(client):
