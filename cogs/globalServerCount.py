928bf337ff48d9c7785f17bbc149bf11299866b8
BioKZM <qRnt@yandex.com>
2022-08-17 14:41:18 +0300

    test

---
+++ cogs/globalServerCount.py
@@ -0,0 +1,16 @@
+from pydoc import cli
+import disnake
+from disnake.ext import commands
+
+
+class GlobalServerCount(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_guild_join(self,guild):
+        channel = self.client.get_channel(1009425217669582969)
+        await channel.send(f"**{guild}** sunucusuna davet edildim.")
+
+def setup(client):
+    client.add_cog(GlobalServerCount)
