a3703c9b17e202efd06f7aad21e16ff402693507
qRnt@yandex.com <qRnt@yandex.com>
2022-08-20 16:41:38 +0300

    Trying to add footer icon

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ client = commands.Bot(command_prefix="!",intents = disnake.Intents.all(), help_c
 
 @client.event
 async def on_ready():
-    print("Entropy is ready!")
-    print(client.user)
+    print(f"{client.user} is ready!")
     await client.change_presence(status = disnake.Status.online,activity = disnake.Game("Creating Chaos..."))
 
 
